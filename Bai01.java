ad08b33f870391a8f1f62d745b7da15863410996
Namheoco123 <nambluetretrauvl@gmail.com>
2025-02-24 13:45:49 +0700

    Add files via upload

---
+++ Bai01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        String ten;
+        int ngaysinh;
+        int mssv;
+        Scanner nhap = new Scanner(System.in);
+        System.out.printf("Nhap ho va ten : ");
+        ten = nhap.nextLine();
+        System.err.printf("Nhap ngay sinh :");
+        ngaysinh = nhap.nextInt();
+        System.out.printf("Nhap MSSV : ");
+        mssv = nhap.nextInt();
+        System.out.println("Ho va ten : "+ten);
+        System.out.println("Ngay sinh : "+ngaysinh);
+        System.out.println("Ma so sinh vien : "+mssv);
+    }
+}
